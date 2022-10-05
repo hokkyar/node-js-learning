517c76b3041e4baeba9374849c1f381858c5cf4e
Hokky Aryasta <hokkyaryasta@gmail.com>
2022-10-05 10:41:06 +0800

    materi 14

---
+++ 14-express-middleware/app.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const app = express()
+const expressEjsLayouts = require('express-ejs-layouts')
+
+app.set('view engine', 'ejs')
+app.use(expressEjsLayouts)
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    const mahasiswa = [
+        {
+            nama: 'Hokky',
+            email: 'hokky@gmail.com'
+        },
+        {
+            nama: 'Doddy',
+            email: 'doddy@gmail.com'
+        },
+        {
+            nama: 'Agus',
+            email: 'agus@gmail.com'
+        },
+    ]
+    res.render('index', {
+        layout: 'layouts/main-layout',
+        title: 'Home',
+        mahasiswa,
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        layout: 'layouts/main-layout',
+        title: 'About'
+    })
+})
+
+app.get('/contact', (req, res) => {
+    res.render('contact', {
+        layout: 'layouts/main-layout',
+        title: 'Contact'
+    })
+})
+
+app.get('/product/:id', (req, res) => {
+    res.send(`Product ID: ${req.params.id} <br> Category: ${req.query.category}`)
+})
+
+app.use('/', (req, res) => {
+    res.status(404).send('404 Not Found')
+})
+
+const PORT = 3002
+app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`))
